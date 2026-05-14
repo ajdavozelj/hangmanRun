11815b4e9b0ea115d0182eb7b0a483103a2e793f
Zoja Skok <skokzoja@gmail.com>
2026-05-14 20:06:28 +0200

    debuged

--- vislice.py
+++ vislice.py
@@ -3,6 +3,21 @@ import requests
 import dotenv
 import os
 
+pygame.init()
+pygame.mixer.init()
+ozadje = pygame.image.load("dungeon.jpg")
+ozadje = pygame.transform.scale(ozadje, (800, 800))
+monster = pygame.image.load("monster.png")
+#monster =  pygame.transform.scale(monster, (,))
+hero = pygame.image.load("hero.png")
+#hero =  pygame.transform.scale(hero, (,))
+hero_back = pygame.image.load("hero_back.png")
+#hero_back =  pygame.transform.scale(hero_back, (,))
+canvas = pygame.display.set_mode((800, 800))
+
+pygame.display.set_caption("Vislice")
+
+#--------API------------------------
 
 API_KEY = os.getenv("API_KEY")
 Navodila = "tell me random english word on B1 level and without explanation or any other text or signs (so no *) "
@@ -26,25 +41,6 @@ def pogovor_z_llm():
 
 dotenv.load_dotenv()
 pogovor_z_llm()
-
-
-
-
-pygame.init()
-pygame.mixer.init()
-ozadje = pygame.image.load("dungeon.jpg")
-ozadje = pygame.transform.scale(ozadje, (800, 800))
-monster = image.load("monster3.png")
-#monster =  pygame.transform.scale(monster, (,))
-hero = pygame.image.load("hero.png")
-#hero =  pygame.transform.scale(hero, (,))
-hero_back = pygame.image.load("hero_back.png")
-#hero_back =  pygame.transform.scale(hero_back, (,))
-canvas = pygame.display.set_mode((800, 800))
-
-pygame.display.set_caption("Vislice")
-
-color = (0, 0, 0)
 
 
 exit = False
