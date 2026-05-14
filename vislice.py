204fb03aafd06ce6ec4a4c07ba9296dd973b20b5
Zoja Skok <skokzoja@gmail.com>
2026-05-14 20:01:46 +0200

    visual base

--- vislice.py
+++ vislice.py
@@ -4,8 +4,16 @@ import dotenv
 import os
 
 pygame.init()
-
-canvas = pygame.display.set_mode((500, 500))
+pygame.mixer.init()
+ozadje = pygame.image.load("dungeon.jpg")
+ozadje = pygame.transform.scale(ozadje, (800, 800))
+monster = image.load("monster3.png")
+#monster =  pygame.transform.scale(monster, (,))
+hero = pygame.image.load("hero.png")
+#hero =  pygame.transform.scale(hero, (,))
+hero_back = pygame.image.load("hero_back.png")
+#hero_back =  pygame.transform.scale(hero_back, (,))
+canvas = pygame.display.set_mode((800, 800))
 
 pygame.display.set_caption("Vislice")
 
@@ -15,7 +23,7 @@ color = (0, 0, 0)
 exit = False
 
 while not exit:
-	canvas.fill(color)
+	canvas.blit(ozadje, (0, 0))
 
 
 	for event in pygame.event.get():
